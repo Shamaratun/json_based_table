b0cc159a2760c1e506c0302d78dbace1fdef59f1
Shamaratun <shamaratunmeme@gmail.com>
2025-10-08 13:20:08 +0600

    responsive tab

--- src/app/reusableTabs/patientTab.tsx
+++ src/app/reusableTabs/patientTab.tsx
@@ -18,24 +18,25 @@ interface ReusableTabsProps {
 
 function ReusableTabs({ tabs }: ReusableTabsProps) {
   return (
-    <div className="w-full overflow-visible">
-  <TabsList className="grid grid-cols-3 sm:grid-cols-3 gap-2 sm:gap-4 w-full">
-    {tabs.map((tab) => {
-      const Icon = tab.icon;
-      return (
-        <TabsTrigger
-          key={tab.value}
-          value={tab.value}
-          className={`flex items-center gap-2 px-4 py-2 text-sm sm:text-base text-gray-800 rounded-lg  transition
-            ${tab.hoverColor || "hover:bg-gray-200"}`}
-            style={{ minHeight: "50px" }}
-        >
-          <Icon size={20} /> {tab.label}
-        </TabsTrigger>
-      );
-    })}
-  </TabsList>
-</div>
+    <div className="w-full mb-4 overflow-visible">
+      <TabsList className="flex flex-wrap gap-2 w-full">
+        {tabs.map((tab) => {
+          const Icon = tab.icon;
+          return (
+            <TabsTrigger
+              key={tab.value}
+              value={tab.value}
+              className={`flex items-center gap-2 px-2 py-2 sm:px-3 sm:py-3 md:px-4 md:py-4 text-sm sm:text-base md:text-lg lg:text-xl text-gray-800 rounded-sm whitespace-nowrap
+          ${tab.bgColor || "bg-gray-100"} 
+          ${tab.hoverColor || "hover:bg-gray-200"}`}
+              style={{ minHeight: "50px" }}      >
+              <Icon className="w-4 h-4 sm:w-5 sm:h-5 md:w-6 md:h-6 lg:w-6 lg:h-6" />
+              {tab.label}
+            </TabsTrigger>
+          );
+        })}
+      </TabsList>
+    </div>
   );
 }
 
