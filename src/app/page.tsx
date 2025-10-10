9bf2cc2740fef5a2b9cbbb247f8701bf13b53943
Shamaratun <shamaratunmeme@gmail.com>
2025-10-10 17:12:47 +0600

    done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,35 +8,13 @@ import AdmissionTable from "./admitted/admissionTable";
 
 
 export default function GetPatientWithIdPage() {
-  const [inputId, setInputId] = useState("");
-  const [patientId, setPatientId] = useState<number | null>(null);
-
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-    if (!inputId) return;
-    setPatientId(Number(inputId));
-  };
+ 
 
   return (
     
     <div style={{ padding: "1.5rem" }}>
       <AdmissionTable/>
-      <h1>Search Patient Profile</h1>
-
-      <form onSubmit={handleSubmit} style={{ marginBottom: "1rem" }}>
-        <input
-          type="number"
-          value={inputId}
-          onChange={(e) => setInputId(e.target.value)}
-          placeholder="Enter Patient ID"
-          style={{ padding: "0.5rem", marginRight: "0.5rem" }}
-        />
-        <button type="submit" style={{ padding: "0.5rem 1rem" }}>
-          Search
-        </button>
-      </form>
-
-      {patientId && <GetPatientWithId patient_id={patientId} />}
+     
     </div>
   );
 }
